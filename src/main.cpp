551877e511409dfb65032ce2379462d8870aae30
Mark Hodgson <mark.hodgson@breeze-it.com>
2023-12-08 15:26:45 +0000

    bit of tidying and add tone()

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <Arduino.h>
 #include "LedControl.h"
 
+// pins (and any other constants)
+
+const int buzzerPin = 5;
+
 const int trigPin = 5;
 const int echoPin = 6;
 
@@ -8,6 +12,8 @@ const int ledLoadCsPin = 10;
 const int ledClkPin = 11;
 const int ledDataInPin = 12;
 const int ledNumDevices = 1;
+
+// global variables
 
 long durationMicroSecs;
 long lastBeepAt;
@@ -19,6 +25,10 @@ LedControl ledControl = LedControl(ledDataInPin, ledClkPin, ledLoadCsPin, ledNum
 void detectScan(long duration);
 void showLedNumber(int number);
 
+//
+// -- run once at start
+//
+
 void setup() {
     pinMode(trigPin, OUTPUT);
     pinMode(echoPin, INPUT);
@@ -26,6 +36,10 @@ void setup() {
 
     Serial.println("Starting!");
 }
+
+//
+// -- loop forever...
+//
 
 void loop() {
 
@@ -35,16 +49,22 @@ void loop() {
     digitalWrite(trigPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
-    durationMicroSecs = pulseIn(echoPin, HIGH, 900); // timeout after 0.9 millsecs (about 150mm) - ignores far away and improves perf.
+    // timeout after 0.9 millsecs (about 150mm) - ignores far away and improves perf.
+    // (returns zero if timed out)
+    durationMicroSecs = pulseIn(echoPin, HIGH, 900);
 
     if(durationMicroSecs != 0) {
         detectScan(durationMicroSecs);
     }
 }
 
+//
+// -- our own stuff...
+//
+
 void detectScan(long duration) {
 
-    // calculate distance (v = 340 m/s, divide by 2 because time is for there and back)
+    // calculate distance (Vs = 340 m/s, divide by 2 because time is for there *and* back)
     distanceCm = (float)durationMicroSecs * (340.0 / 2.0 / 10000.0);
 
     Serial.print("Duration: ");
@@ -55,6 +75,7 @@ void detectScan(long duration) {
 
     long sinceLastBeep = lastBeepAt == 0 ? 9999999 : millis() - lastBeepAt;
 
+    // only if close and moving towards sensor
     if(prevDistanceCm > distanceCm && distanceCm <= 3) {
         //Serial.print(sinceLastBeep);
         //Serial.println("  - maybe beep?");
@@ -64,11 +85,12 @@ void detectScan(long duration) {
             Serial.print(sinceLastBeep);
             Serial.println("  @@@ BEEP @@@");
             lastBeepAt = millis();
+            tone(buzzerPin, 2000, 300); // ???
         }
     }
 
+    // remember how close we were to see which direction were moving
     prevDistanceCm = distanceCm;
-
 }
 
 
